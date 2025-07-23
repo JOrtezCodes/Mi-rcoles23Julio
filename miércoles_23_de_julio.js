811711b063f55f28cbd5b858743f3bb7ae1b7bc4
Joel <hectorjoelortez@gmail.com>
2025-07-23 15:03:35 -0600

    inge dinora :)

---
+++ miércoles_23_de_julio.js
@@ -0,0 +1,14 @@
+//CLASE MIÉRCOLES 23 DE JULIO
+//EJERCICIO 1
+let edad = 18
+console.log(edad >= 18 ? "Puede entrar al cine" : "No puede entrar")
+
+//EJERCICIO 2
+let edad2 = 25
+let esDiscapacitado = false
+console.log(edad2 > 60 || esDiscapacitado ? "Tiene descuento" : "No tiene descuento")
+
+//EJERCICIO 3
+let nota = 8
+console.log(nota >= 7 ? "Aprobó" : "No aprobó")
+
